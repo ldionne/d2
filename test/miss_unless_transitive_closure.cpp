1a170d7cb98ab146a99bf99fd7f50afaf45dbf59
Louis Dionne <louis.dionne92@gmail.com>
2012-12-28 07:36:01 -0500

    Add a test to make sure the transitive closure of the `is held while locking' relation is computed correctly.

---
+++ test/miss_unless_transitive_closure.cpp
@@ -0,0 +1,42 @@
+
+#include "mock.hpp"
+#include <d2/logging.hpp>
+
+#include <ostream>
+
+// This deadlock is missed if the "transitive closure" is not computed while
+// building the graph because the B lock makes the A->B->C->A cycle have
+// two edges (A->B)(B->C) within the same thread (t0). If the transitive
+// closure of the `is held while locking' relation is computed during the
+// building of the lock graph, the found cycle is A->C->A instead, which is
+// valid.
+int main() {
+    d2::set_event_sink(&std::cout);
+    d2::enable_event_logging();
+
+    mock_mutex A, B, C;
+
+    mock_thread t0([&] {
+        A.lock();
+            B.lock();
+                C.lock();
+                C.unlock();
+            B.unlock();
+        A.unlock();
+    });
+
+    mock_thread t1([&] {
+        C.lock();
+            A.lock();
+            A.unlock();
+        C.unlock();
+    });
+
+    t0.start();
+    t1.start();
+
+    t1.join();
+    t0.join();
+
+    d2::disable_event_logging();
+}
