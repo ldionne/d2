efd08a9b3b068e16866ef006ce8f374c5e4d0a61
Louis Dionne <louis.dionne92@gmail.com>
2013-01-05 03:15:34 -0500

    Manage streaming directly in the LockDebugInfo and StackFrame classes.

--- include/d2/detail/lock_debug_info.hpp
+++ include/d2/detail/lock_debug_info.hpp
@@ -1,5 +1,5 @@
 /**
- * This file defines several utilities used in the rest of the library.
+ * This file defines the `StackFrame` and the `LockDebugInfo` classes.
  */
 
 #ifndef D2_DETAIL_LOCK_DEBUG_INFO_HPP
@@ -7,12 +7,10 @@
 
 #include <d2/detail/config.hpp>
 
-#include <algorithm>
-#include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/operators.hpp>
 #include <boost/range/begin.hpp>
 #include <boost/range/end.hpp>
-#include <iterator>
+#include <iosfwd>
 #include <string>
 #include <vector>
 
@@ -33,44 +31,26 @@ struct StackFrame : boost::equality_comparable<StackFrame> {
     { }
 
     friend bool operator==(StackFrame const& a, StackFrame const& b) {
-        return a.ip == b.ip && a.function == b.function && a.module==b.module;
+        return a.ip == b.ip && a.function == b.function && a.module == b.module;
     }
 
-    template <typename Ostream>
-    friend Ostream& operator<<(Ostream& os, StackFrame const& self) {
-        os << self.ip << "    " << self.function << " in " << self.module;
-        return os;
-    }
+    D2_API friend std::istream& operator>>(std::istream&, StackFrame&);
+    D2_API friend std::ostream& operator<<(std::ostream&, StackFrame const&);
 };
-} // end namespace detail
-} // end namespace d2
 
-BOOST_FUSION_ADAPT_STRUCT(
-    d2::detail::StackFrame,
-    (void const*, ip)
-    (std::string, function)
-    (std::string, module)
-)
-
-namespace d2 {
-namespace detail {
-
-struct LockDebugInfo : boost::equality_comparable<LockDebugInfo> {
+struct D2_API LockDebugInfo : boost::equality_comparable<LockDebugInfo> {
     typedef std::vector<StackFrame> CallStack;
     CallStack call_stack;
 
-    D2_API void init_call_stack(unsigned int ignore = 0);
+    void init_call_stack(unsigned int ignore = 0);
 
-    friend bool operator==(LockDebugInfo const& a, LockDebugInfo const&b){
+    friend bool operator==(LockDebugInfo const& a, LockDebugInfo const&b) {
         return a.call_stack == b.call_stack;
     }
 
-    template <typename Ostream>
-    friend Ostream& operator<<(Ostream& os, LockDebugInfo const& self) {
-        std::copy(boost::begin(self.call_stack), boost::end(self.call_stack),
-                  std::ostream_iterator<StackFrame>(os, "\n"));
-        return os;
-    }
+    D2_API friend std::ostream& operator<<(std::ostream&, LockDebugInfo const&);
+
+    D2_API friend std::istream& operator>>(std::istream&, LockDebugInfo&);
 };
 
 } // end namespace detail
--- src/lock_debug_info.cpp
+++ src/lock_debug_info.cpp
@@ -4,34 +4,49 @@
 
 #define D2_SOURCE
 #include <d2/detail/config.hpp>
-#include <d2/detail/event_io.hpp>
+#include <d2/detail/lock_debug_info.hpp>
 
 #include <boost/assert.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/spirit/include/karma.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/qi_match.hpp>
 #include <dbg/frames.hpp>
 #include <dbg/symbols.hpp>
 #include <iterator>
+#include <string>
 
+
+BOOST_FUSION_ADAPT_STRUCT(
+    d2::detail::StackFrame,
+    (void const*, ip)
+    (std::string, function)
+    (std::string, module)
+)
 
 namespace d2 {
 namespace detail {
 
-template <typename OutputIterator>
-class StackFrameSink : public dbg::symsink {
-    OutputIterator out_;
+namespace {
+    template <typename OutputIterator>
+    class StackFrameSink : public dbg::symsink {
+        OutputIterator out_;
 
-public:
-    explicit StackFrameSink(OutputIterator const& out) : out_(out) { }
+    public:
+        explicit StackFrameSink(OutputIterator const& out) : out_(out) { }
 
-    virtual void process_function(void const* ip, char const* name,
-                                                  char const* module) {
-        *out_++ = StackFrame(ip, name, module);
-    }
-};
+        virtual void process_function(void const* ip, char const* name,
+                                                      char const* module) {
+            *out_++ = StackFrame(ip, name, module);
+        }
+    };
+} // end anonymous namespace
 
-D2_API void LockDebugInfo::init_call_stack(unsigned int ignore /* = 0 */) {
+void LockDebugInfo::init_call_stack(unsigned int ignore /* = 0 */) {
     dbg::call_stack<100> stack;
     dbg::symdb symbols;
     stack.collect(ignore + 1); // ignore our frame
+    call_stack.reserve(stack.size());
 
     StackFrameSink<std::back_insert_iterator<CallStack> >
                                     sink(std::back_inserter(call_stack));
@@ -41,6 +56,64 @@ D2_API void LockDebugInfo::init_call_stack(unsigned int ignore /* = 0 */) {
     BOOST_ASSERT_MSG(call_stack.size() == stack.size(),
                     "not all the frames from the dbg::call_stack "
                     "were copied to this->call_stack");
+}
+
+D2_API extern std::ostream& operator<<(std::ostream& os,
+                                       StackFrame const& self) {
+    using namespace boost::spirit::karma;
+
+    os << format(
+
+            hex             // ip
+        <<  '$'
+        <<  +~char_('$')    // function
+        <<  '$'
+        <<  +~char_('\n')   // module
+        <<  '\n'
+
+        , self);
+
+    return os;
+}
+
+D2_API extern std::istream& operator>>(std::istream& is, StackFrame& self) {
+    using namespace boost::spirit::qi;
+
+    stream_parser<char, void*> voidp;
+    is >> match(
+
+            as<void*>()[voidp]  // ip
+        >>  '$'
+        >>  +~char_('$')        // function
+        >>  '$'
+        >>  +~char_('\n')       // module
+        >>  '\n'
+
+        , self);
+
+    return is;
+}
+
+D2_API extern std::ostream& operator<<(std::ostream& os,
+                                       LockDebugInfo const& self) {
+    using namespace boost::spirit::karma;
+
+    os << format("[[" << *stream << "]]", self.call_stack);
+
+    return os;
+}
+
+D2_API extern std::istream& operator>>(std::istream& is,
+                                       LockDebugInfo& self) {
+    using namespace boost::spirit::qi;
+
+    stream_parser<char, StackFrame> stack_frame;
+    // Note: The lexeme[] directive is useless because there is no skipper
+    //       anyways, but it is required because we can't apply the kleene
+    //       star to a stream_parser directly (probably a bug).
+    is >> match("[[" >> *lexeme[stack_frame] >> "]]", self.call_stack);
+
+    return is;
 }
 
 } // end namespace detail
