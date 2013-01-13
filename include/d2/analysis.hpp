d54f8321520254d9abcdb346979167066f54aff3
Louis Dionne <louis.dionne92@gmail.com>
2013-01-12 19:02:29 -0500

    Refactor the analysis algorithm.

--- include/d2/analysis.hpp
+++ include/d2/analysis.hpp
@@ -5,129 +5,17 @@
 #ifndef D2_ANALYSIS_HPP
 #define D2_ANALYSIS_HPP
 
-#include <d2/lock_graph.hpp>
-#include <d2/segment.hpp>
-#include <d2/segmentation_graph.hpp>
-#include <d2/sync_object.hpp>
-#include <d2/thread.hpp>
+#include <d2/detail/all_cycles.hpp>
 
-#include <boost/assert.hpp>
-#include <boost/concept_check.hpp>
 #include <boost/foreach.hpp>
-#include <boost/graph/depth_first_search.hpp>
 #include <boost/graph/graph_traits.hpp>
-#include <boost/graph/graph_utility.hpp>
-#include <boost/optional.hpp>
+#include <boost/graph/properties.hpp>
 #include <boost/range/begin.hpp>
 #include <boost/range/end.hpp>
-#include <boost/unordered_map.hpp>
-#include <deque>
-#include <set>
-#include <vector>
 
 
 namespace d2 {
 namespace detail {
-
-// Note: You can enable/disable this to debug the algorithm.
-#define D2_DEBUG_ALL_CYCLES_DUMB(statement) do { } while (false)
-// #define D2_DEBUG_ALL_CYCLES_DUMB(statement) do { statement; } while (false)
-
-/**
- * Wrapper visitor for use within the `all_cycles_dumb` algorith. It allows
- * the wrapped visitor to keep the same interface as for `tiernan_all_cycles`.
- */
-template <typename Adapted, typename Graph>
-class AllCyclesWrapper : public boost::dfs_visitor<> {
-    Adapted visitor_;
-    typedef typename boost::graph_traits<Graph>::edge_descriptor Edge;
-    typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
-
-    typedef boost::unordered_map<Vertex, Edge> PredecessorMap;
-    PredecessorMap predecessors_;
-    std::set<std::deque<Edge> >& seen_cycles;
-
-public:
-    explicit AllCyclesWrapper(Adapted const& v, Graph const&,
-                              std::set<std::deque<Edge> >& seen)
-        : visitor_(v), seen_cycles(seen)
-    { }
-
-    void tree_edge(Edge e, Graph const& g) {
-        D2_DEBUG_ALL_CYCLES_DUMB(std::cout <<
-            "tree edge: " << e << '\n' <<
-            "set predecessor of " << target(e, g) << " to " << e << '\n');
-        predecessors_[target(e, g)] =  e;
-    }
-
-    void back_edge(Edge e, Graph const& g) {
-        D2_DEBUG_ALL_CYCLES_DUMB(std::cout <<
-            "back edge: " << e << '\n' <<
-            "making sure " << source(e, g) << " has a predecessor\n");
-        BOOST_ASSERT_MSG(
-            predecessors_.find(source(e,g)) != boost::end(predecessors_),
-            "the predecessor edge of the source of the current edge is not "
-            "defined, something's wrong");
-
-        // Using the predecessor map maintained by the
-        // edge_predecessor_recorder, we create a path of the form:
-        // (u, v) (v, w) (w, x) ...
-        // Representing the edges forming the cycle. We then call the adapted
-        // visitor with that path, which is much easier to manipulate.
-        std::deque<Edge> cycle;
-        cycle.push_front(e);
-        typedef typename PredecessorMap::const_iterator PredecessorIterator;
-        while (true) {
-            PredecessorIterator it = predecessors_.find(source(e, g));
-            if (it == boost::end(predecessors_))
-                break;
-
-            cycle.push_front(e = it->second);
-        }
-
-        // Since it is possible to have several connected components in the
-        // graph, we must make sure we do not call the visitor with redundant
-        // cycles that were already found in a previous search.
-        if (seen_cycles.insert(cycle).second)
-            visitor_.cycle(cycle, g);
-    }
-};
-
-/**
- * Bad algorithm to compute all the cycles in a graph. It first does a depth
- * first search and detects the cycles in the graph. Then, it starts over a
- * depth first search at each vertex implicated in a cycle found during the
- * first pass.
- */
-template <typename Graph, typename Visitor>
-void all_cycles_dumb(Graph const& g, Visitor const& vis) {
-    typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
-    typedef typename boost::graph_traits<Graph>::vertex_iterator VertexIter;
-    typedef typename boost::graph_traits<Graph>::edge_descriptor Edge;
-
-    std::set<std::deque<Edge> > seen_cycles;
-    AllCyclesWrapper<Visitor, Graph> wrapper(vis, g, seen_cycles);
-
-    VertexIter first, last;
-    boost::tie(first, last) = vertices(g);
-    boost::depth_first_search(g, boost::root_vertex(*first).visitor(wrapper));
-
-    // Find all vertices implicated in cycles.
-    std::set<Vertex> hot_vertices;
-    BOOST_FOREACH(std::deque<Edge> const& cycle, seen_cycles) {
-        hot_vertices.insert(source(cycle[0], g));
-        BOOST_FOREACH(Edge const& edge, cycle)
-            hot_vertices.insert(target(edge, g));
-    }
-
-    // Start over a depth-first search at every vertex implicated in a cycle.
-    // This allows us to find all the different cycles in the directed graph.
-    // Let's say the first dfs found a->b->a;
-    // the subsequent searches will find b->a->b
-    hot_vertices.erase(*first); // We already visited that.
-    BOOST_FOREACH(Vertex v, hot_vertices)
-        boost::depth_first_search(g, boost::root_vertex(v).visitor(wrapper));
-}
 
 /**
  * Return whether two unordered containers have a non-empty intersection.
@@ -145,45 +33,44 @@ bool unordered_intersects(Unordered1 const& a, Unordered2 const& b) {
 
 /**
  * Wrap a `BinaryFunction` to implement a visitor for the goodlock algorithm.
- * @todo If we use an adjacency_matrix to store the segmentation graph, we
- *       should compute its transitive closure to reduce the complexity of
- *       the happens-before relation.
+ *
+ * @internal If we use an adjacency_matrix to store the segmentation graph, we
+ *           should compute its transitive closure to reduce the complexity of
+ *           the happens-before relation.
  */
 template <typename LockGraph, typename SegmentationGraph, typename Function>
 class CycleVisitor {
-    typedef typename boost::graph_traits<LockGraph>::edge_descriptor
-                                                    LockGraphEdgeDescriptor;
-    typedef typename boost::graph_traits<LockGraph>::vertex_descriptor
-                                                    LockGraphVertexDescriptor;
-    typedef std::vector<LockGraphEdgeDescriptor> EdgePath;
+    typedef boost::graph_traits<LockGraph> GraphTraits;
+    typedef typename GraphTraits::edge_descriptor LockGraphEdgeDescriptor;
 
     // Property map to access the edge labels of the lock graph.
-    typedef typename boost::property_map<LockGraph,
-                               boost::edge_bundle_t>::const_type EdgeLabelMap;
+    typedef typename boost::property_map<
+                LockGraph, boost::edge_bundle_t
+            >::const_type EdgeLabelMap;
+
     SegmentationGraph const& sg_;
-    Function& f_;
+    Function f_;
 
 public:
-    CycleVisitor(SegmentationGraph const& sg, Function& f)
+    CycleVisitor(SegmentationGraph const& sg, Function const& f)
         : sg_(sg), f_(f)
     { }
 
     /**
-     * Function called whenever a cycle is found. It calls the wrapped
-     * function with a sequence containing the edges in the cycle and a
-     * constant reference to the lock graph, but only if the cycle respects
-     * certain conditions, i.e. if the cycle represents a deadlock in the
-     * lock graph.
+     * Method called whenever a cycle is found.
+     *
+     * If the cycle respects certain conditions, calls the wrapped
+     * `BinaryFunction` with a sequence of unspecified type containing the
+     * edges in the cycle and a constant reference to the lock graph.
+     *
+     * @note The conditions are those that make a cycle a potential deadlock
+     *       in the lock graph.
      */
     template <typename EdgePath>
     void cycle(EdgePath const& edge_path, LockGraph const& graph) const {
-        D2_DEBUG_ALL_CYCLES_DUMB(
-            std::cout << "Found cycle: ";
-            std::copy(boost::begin(edge_path), boost::end(edge_path),
-                std::ostream_iterator<typename EdgePath::value_type>(
-                    std::cout, " "));
-            std::cout << '\n';
-        );
+        // These are the conditions for a cycle to be a valid
+        // potential deadlock:
+
         // For any given pair of edges (e1, e2)
         EdgeLabelMap labels = get(boost::edge_bundle, graph);
         BOOST_FOREACH(LockGraphEdgeDescriptor e1, edge_path) {
@@ -214,12 +101,14 @@ public:
  * Analyze the lock graph and the segmentation graph to determine whether the
  * program execution represented by them contains a deadlock. `f` is called
  * whenever a potential deadlock is detected.
+ *
  * @see `detail::CycleVisitor` for more details.
  */
 template <typename LockGraph, typename SegmentationGraph, typename Function>
-void analyze(LockGraph const& lg, SegmentationGraph const& sg, Function f) {
+void analyze(LockGraph const& lg, SegmentationGraph const& sg,
+             Function const& f) {
     detail::CycleVisitor<LockGraph, SegmentationGraph, Function> vis(sg, f);
-    detail::all_cycles_dumb(lg, vis);
+    detail::all_cycles(lg, vis);
 }
 
 } // end namespace d2
---
+++ include/d2/detail/all_cycles.hpp
@@ -0,0 +1,171 @@
+/**
+ * This file defines the `all_cycles` graph algorithm to find all the
+ * cycles in a directed graph.
+ */
+
+#ifndef D2_DETAIL_ALL_CYCLES_HPP
+#define D2_DETAIL_ALL_CYCLES_HPP
+
+#include <boost/assert.hpp>
+#include <boost/concept/assert.hpp>
+#include <boost/concept_check.hpp>
+#include <boost/foreach.hpp>
+#include <boost/graph/depth_first_search.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/range/begin.hpp>
+#include <boost/range/end.hpp>
+#include <boost/unordered_map.hpp>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <vector>
+
+
+namespace d2 {
+namespace detail {
+
+// Note: You can enable/disable this to debug the algorithm. Some information
+//       will be printed to stdout.
+#define D2_DEBUG_ALL_CYCLES(statement) do { } while (false)
+// #define D2_DEBUG_ALL_CYCLES(statement) do { statement; } while (false)
+
+/**
+ * Wrapper visitor for use within the `all_cycles` algorithm.
+ *
+ * Its use is twofold:
+ *  Keep track of the cycles seen so far, so the `all_cycles` algorithm can
+ *  start its dfs only on vertices implicated in a cycle.
+ *
+ *  Call a visitor complying to the `boost::tiernan_all_cycles` visitor
+ *  interface on every cycle encountered.
+ */
+template <typename Visitor, typename Graph, typename SeenCycles>
+class AllCyclesWrapper : public boost::dfs_visitor<> {
+    Visitor visitor_;
+    typedef typename boost::graph_traits<Graph>::edge_descriptor Edge;
+    typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
+    typedef typename SeenCycles::value_type Cycle;
+    BOOST_CONCEPT_ASSERT((boost::FrontInsertionSequenceConcept<Cycle>));
+
+    typedef boost::unordered_map<Vertex, Edge> PredecessorMap;
+    PredecessorMap predecessors_;
+
+    SeenCycles& seen_cycles;
+
+public:
+    AllCyclesWrapper(Visitor const& v, Graph const&, SeenCycles& seen)
+        : visitor_(v), seen_cycles(seen)
+    { }
+
+    void tree_edge(Edge e, Graph const& g) {
+        D2_DEBUG_ALL_CYCLES(std::cout <<
+            "tree edge: " << e << '\n' <<
+            "set predecessor of " << target(e, g) << " to " << e << '\n');
+        predecessors_[target(e, g)] =  e;
+    }
+
+    void back_edge(Edge e, Graph const& g) {
+        D2_DEBUG_ALL_CYCLES(std::cout <<
+            "back edge: " << e << '\n' <<
+            "making sure " << source(e, g) << " has a predecessor\n");
+        BOOST_ASSERT_MSG(
+            predecessors_.find(source(e,g)) != boost::end(predecessors_),
+            "the predecessor edge of the source of the current edge is not "
+            "defined, something's wrong");
+
+        // Using the predecessor map maintained by the
+        // edge_predecessor_recorder, we create a path of the form:
+        // (u, v) (v, w) (w, x) ...
+        // Representing the edges forming the cycle. We then call the adapted
+        // visitor with that path, which is much easier to manipulate.
+        Cycle cycle;
+        cycle.push_front(e);
+        typedef typename PredecessorMap::const_iterator PredecessorIterator;
+        while (true) {
+            PredecessorIterator it = predecessors_.find(source(e, g));
+            if (it == boost::end(predecessors_))
+                break;
+
+            cycle.push_front(e = it->second);
+        }
+
+        // Since it is possible to have several connected components in the
+        // graph, we must make sure we do not call the visitor with redundant
+        // cycles that were already found in a previous search.
+        if (seen_cycles.insert(cycle).second) {
+            D2_DEBUG_ALL_CYCLES(
+                std::cout << "Found cycle: ";
+                std::copy(boost::begin(cycle), boost::end(cycle),
+                    std::ostream_iterator<Edge>(std::cout, " "));
+                std::cout << '\n';
+            );
+            visitor_.cycle(cycle, g);
+        }
+    }
+};
+
+/**
+ * Dumb algorithm to compute all the cycles in a graph.
+ *
+ * It first does a depth first search and detects the cycles in the graph.
+ * Then, it starts over a depth first search at each vertex implicated in a
+ * cycle found during the first pass to find the cycles in every possible
+ * direction.
+ *
+ * The visitor's `cycle` method is called with a constant reference to a
+ * sequence of unspecified type and a constant reference to the graph being
+ * visited.
+ */
+template <typename Graph, typename Visitor>
+void all_cycles(Graph const& g, Visitor const& visitor) {
+    typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
+    typedef typename boost::graph_traits<Graph>::vertex_iterator VertexIter;
+    typedef typename boost::graph_traits<Graph>::edge_descriptor Edge;
+    typedef std::deque<Edge> Cycle;
+
+    D2_DEBUG_ALL_CYCLES(std::cout <<
+        "starting the all_cycles algorithm\n"
+    <<  "    graph has " << num_vertices(g) << " vertices\n");
+
+    if (num_vertices(g) == 0)
+        return;
+
+    std::set<Cycle> seen_cycles;
+    typedef AllCyclesWrapper<Visitor, Graph, std::set<Cycle> > Wrapper;
+    Wrapper wrapper(visitor, g, seen_cycles);
+
+    // Perform a first dfs to find vertices implicated in a cycle.
+    Vertex first_vertex = *vertices(g).first;
+    boost::depth_first_search(g,
+        boost::root_vertex(first_vertex).visitor(wrapper));
+
+    D2_DEBUG_ALL_CYCLES(std::cout <<
+        "first dfs done\n"
+    <<  "    found " << seen_cycles.size() << " cycles\n");
+
+    // Find all vertices implicated in a cycle.
+    std::set<Vertex> vertices_in_a_cycle;
+    BOOST_FOREACH(Cycle const& cycle, seen_cycles) {
+        vertices_in_a_cycle.insert(source(cycle[0], g));
+        BOOST_FOREACH(Edge const& edge, cycle)
+            vertices_in_a_cycle.insert(target(edge, g));
+    }
+
+    D2_DEBUG_ALL_CYCLES(std::cout <<
+    "    involving " << vertices_in_a_cycle.size() << " different vertices\n");
+
+    // Start over a depth-first search at every vertex implicated in a cycle.
+    // This allows us to find all the different cycles in the directed graph.
+    // Let's say the first dfs found a->b->a;
+    // the subsequent searches will find b->a->b
+    vertices_in_a_cycle.erase(first_vertex); // We already visited that one
+    BOOST_FOREACH(Vertex v, vertices_in_a_cycle)
+        boost::depth_first_search(g, boost::root_vertex(v).visitor(wrapper));
+}
+
+} // end namespace detail
+} // end namespace d2
+
+#endif // !D2_DETAIL_ALL_CYCLES_HPP
