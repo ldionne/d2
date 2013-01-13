6bcc7f2464a85a12d59c0c7268305ddada35c93d
Louis Dionne <louis.dionne92@gmail.com>
2013-01-12 21:39:09 -0500

    Reformulate comment about computing the transitive closure when the lock graph is built.

--- test/integration/miss_unless_transitive_closure.cpp
+++ test/integration/miss_unless_transitive_closure.cpp
@@ -2,12 +2,13 @@
 #include "mock.hpp"
 
 
-// This deadlock is missed if the "transitive closure" is not computed while
-// building the graph because the B lock makes the A->B->C->A cycle have
-// two edges (A->B)(B->C) within the same thread (t0). If the transitive
-// closure of the `is held while locking' relation is computed during the
-// building of the lock graph, the found cycle is A->C->A instead, which is
-// valid.
+// This deadlock is missed if the transitive closure of the
+// `X is held by this thread while locking Y' relation is not computed when
+// building the lock graph because the B lock causes the (A, B)(B, C)(C, A)
+// cycle to have two edges (A, B)(B, C) within the same thread (t0). If the
+// transitive closure is computed, the cycle we find is (A, C)(C, A) instead,
+// which is valid. Thus, the transitive closure allows us to "jump" over the
+// B lock because there is a DIRECT edge from A to C.
 int main(int argc, char const* argv[]) {
     mock::begin_integration_test(argc, argv, __FILE__);
 
