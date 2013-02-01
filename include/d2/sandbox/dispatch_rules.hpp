7c36143cb7e15e5eec6372fa8059312622f4b34c
Louis Dionne <louis.dionne92@gmail.com>
2013-01-31 19:09:11 -0500

    Add a callable interface to the dispatch_rules.

--- include/d2/sandbox/dispatch_rules.hpp
+++ include/d2/sandbox/dispatch_rules.hpp
@@ -5,16 +5,42 @@
 #ifndef D2_SANDBOX_DISPATCH_RULES_HPP
 #define D2_SANDBOX_DISPATCH_RULES_HPP
 
+#include <d2/sandbox/event_traits.hpp>
+
+#include <boost/mpl/assert.hpp>
+#include <boost/mpl/if.hpp>
 #include <boost/preprocessor/repetition/enum_params.hpp>
 #include <boost/preprocessor/repetition/enum_params_with_a_default.hpp>
-#include <boost/proto/matches.hpp>
-#include <boost/proto/proto_fwd.hpp>
+#include <boost/proto/proto.hpp>
+#include <boost/utility/result_of.hpp>
 
 
 namespace d2 {
 namespace sandbox {
 
-typedef boost::proto::_expr _event;
+struct _event : boost::proto::transform<_event> {
+    template <typename Expr, typename State, typename Data>
+    struct impl : boost::proto::transform_impl<Expr, State, Data> {
+        typedef typename boost::mpl::if_<
+                    is_event<typename impl::expr>,
+                    boost::proto::_expr,
+                    boost::proto::_value
+                >::type unwrap_event;
+
+        typedef typename boost::result_of<
+                    unwrap_event(typename impl::expr_param,
+                                 typename impl::state_param,
+                                 typename impl::data_param)
+                >::type result_type;
+
+        result_type operator()(typename impl::expr_param e,
+                               typename impl::state_param s,
+                               typename impl::data_param d) const {
+            return unwrap_event()(e, s, d);
+        }
+    };
+};
+
 using boost::proto::_;
 using boost::proto::_data;
 using boost::proto::_state;
@@ -25,6 +51,7 @@ using boost::proto::not_;
 using boost::proto::or_;
 using boost::proto::when;
 
+
 #define D2_MAX_DISPATCH_RULES_ARITY BOOST_PROTO_MAX_LOGICAL_ARITY
 
 template <
@@ -32,11 +59,36 @@ template <
         D2_MAX_DISPATCH_RULES_ARITY, typename A, not_<_>
     )
 >
-struct dispatch_rules
-    : or_<
-        BOOST_PP_ENUM_PARAMS(D2_MAX_DISPATCH_RULES_ARITY, A)
-    >
-{ };
+class dispatch_rules
+    : public or_<BOOST_PP_ENUM_PARAMS(D2_MAX_DISPATCH_RULES_ARITY, A)>
+{
+    typedef typename dispatch_rules::proto_grammar Base;
+
+public:
+    template <typename Event>
+    void operator()(Event const& e) const {
+        typedef typename boost::proto::terminal<Event>::type Terminal;
+        BOOST_MPL_ASSERT((matches<Terminal, dispatch_rules>));
+        Terminal term = {e};
+        Base::operator()(term);
+    }
+
+    template <typename Event, typename State>
+    void operator()(Event const& e, State const& s) const {
+        typedef typename boost::proto::terminal<Event>::type Terminal;
+        BOOST_MPL_ASSERT((matches<Terminal, dispatch_rules>));
+        Terminal term = {e};
+        Base::operator()(term, s);
+    }
+
+    template <typename Event, typename State, typename Data>
+    void operator()(Event const& e, State const& s, Data& d) const {
+        typedef typename boost::proto::terminal<Event>::type Terminal;
+        BOOST_MPL_ASSERT((matches<Terminal, dispatch_rules>));
+        Terminal term = {e};
+        Base::operator()(term, s, d);
+    }
+};
 
 } // end namespace sandbox
 } // end namespace d2
