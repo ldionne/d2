2abdf45af63ae29144717beaf76f1e0d286952a5
Louis Dionne <louis.dionne92@gmail.com>
2013-03-11 18:22:03 -0400

    Fix the ABBA_redundant_diff_functions test.

--- test/scenarios/ABBA_redundant_diff_functions.cpp
+++ test/scenarios/ABBA_redundant_diff_functions.cpp
@@ -1,11 +1,13 @@
 /**
  * This test makes sure that the same locking pattern repeated in two
  * different functions will still trigger two different deadlock potentials.
- * For example, f() and g() perform the exact same thing. If we build the
- * graph/do the analysis naively, we could end up ignoring one of these two
- * deadlock potentials because one of them seems redundant. However, since
- * they happen in two different functions, it is pertinent to report both
- * deadlock potentials because it may be non obvious in real code.
+ *
+ * For example, t0 performs the same thing in its top level and inside the
+ * lambda function. If we build the graph/do the analysis naively, we could
+ * end up ignoring one of these two deadlock potentials because one of them
+ * seems redundant. However, since they happen in two different functions,
+ * it is pertinent to report both deadlock potentials because it may be non
+ * obvious in real code.
  */
 
 #include <d2mock.hpp>
@@ -14,7 +16,7 @@
 int main(int argc, char const* argv[]) {
     d2mock::mutex A, B;
 
-    auto f = [&] {
+    d2mock::thread t0([&] {
         A.lock();
             B.lock();
             B.unlock();
@@ -27,16 +29,14 @@ int main(int argc, char const* argv[]) {
                 B.unlock();
             A.unlock();
         }();
-    };
+    });
 
-    auto g = [&] {
+    d2mock::thread t1([&] {
         B.lock();
             A.lock();
             A.unlock();
         B.unlock();
-    };
-
-    d2mock::thread t0(f), t1(g);
+    });
 
     auto test_main = [&] {
         t0.start();
@@ -50,11 +50,11 @@ int main(int argc, char const* argv[]) {
     // to each deadlock should also be different, but we don't check that now.
     return d2mock::check_scenario(test_main, argc, argv, {
                 {
-                    {t0, B, A},
-                    {t1, A, B}
+                    {t0, A, B},
+                    {t1, B, A}
                 }, {
-                    {t0, B, A},
-                    {t1, A, B}
+                    {t0, A, B},
+                    {t1, B, A}
                 }
             });
 }
