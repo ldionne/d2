3781821ffcf0c012320f6cb7538212dfff8316db
Louis Dionne <louis.dionne92@gmail.com>
2013-03-24 16:17:01 -0400

    Micro-optimize the hawick_circuits algorithm.

--- include/d2/detail/hawick_circuits.hpp
+++ include/d2/detail/hawick_circuits.hpp
@@ -13,6 +13,11 @@
 #include <boost/graph/one_bit_color_map.hpp>
 #include <boost/graph/properties.hpp>
 #include <boost/move/utility.hpp>
+#include <boost/property_map/property_map.hpp>
+#include <boost/range/begin.hpp>
+#include <boost/range/end.hpp>
+#include <boost/range/iterator.hpp>
+#include <boost/tuple/tuple.hpp> // for boost::tie
 #include <boost/type_traits/remove_reference.hpp>
 #include <boost/utility/result_of.hpp>
 #include <set>
@@ -87,6 +92,8 @@ template <
     typename Graph,
     typename Visitor,
     typename VertexIndexMap,
+    typename Stack,
+    typename ClosedMatrix,
     typename GetAdjacentVertices
 >
 struct hawick_circuits_from {
@@ -98,25 +105,63 @@ private:
     typedef typename boost::property_traits<
                 VertexIndexMap
             >::reference VertexIndex;
+
     typedef typename boost::result_of<
                 GetAdjacentVertices(Vertex, Graph const&)
             >::type AdjacentVertices;
+    typedef typename boost::range_iterator<
+                AdjacentVertices
+            >::type AdjacencyIterator;
 
+    // The one_bit_color_map starts all white, i.e. not blocked.
+    // Since we make that assumption (I looked at the implementation, but
+    // I can't find anything that documents this behavior), we're gonna
+    // assert it in the constructor.
     typedef boost::one_bit_color_map<VertexIndexMap> BlockedMap;
-    typedef boost::one_bit_color_type BlockedColor;
-    static BlockedColor const blocked_false = boost::one_bit_white;
-    static BlockedColor const blocked_true = boost::one_bit_not_white;
+    typedef typename boost::property_traits<
+                BlockedMap
+            >::value_type BlockedColor;
 
-    typedef std::vector<Vertex> Stack;
-    typedef std::vector<std::vector<Vertex> > ClosedMatrix;
+    static BlockedColor blocked_false_color()
+    { return boost::color_traits<BlockedColor>::white(); }
+
+    static BlockedColor blocked_true_color()
+    { return boost::color_traits<BlockedColor>::black(); }
+
+    // This is used by the constructor to secure the assumption
+    // documented above.
+    bool blocked_map_starts_all_unblocked() const {
+        BOOST_FOREACH(Vertex v, vertices(graph_))
+            if (is_blocked(v))
+                return false;
+        return true;
+    }
+
+    // This is only used in the constructor to make sure the optimization of
+    // sharing data structures between iterations does not break the code.
+    bool all_closed_rows_are_empty() const {
+        BOOST_FOREACH(typename ClosedMatrix::reference row, closed_)
+            if (!row.empty())
+                return false;
+        return true;
+    }
 
 public:
     hawick_circuits_from(Graph const& graph, Visitor& visitor,
-                         VertexIndexMap const& vim, VerticesSize n_vertices)
-        : graph_(graph), visitor_(visitor), vim_(vim),
-          blocked_(n_vertices, vim_), closed_(n_vertices)
+                         VertexIndexMap const& vim,
+                         Stack& stack, ClosedMatrix& closed,
+                         VerticesSize n_vertices)
+        : graph_(graph), visitor_(visitor), vim_(vim), stack_(stack),
+          closed_(closed), blocked_(n_vertices, vim_)
         {
-            stack_.reserve(n_vertices);
+            BOOST_ASSERT(blocked_map_starts_all_unblocked());
+
+            // Since sharing the data structures between iterations is
+            // just an optimization, it must always be equivalent to
+            // constructing new ones in this constructor.
+            BOOST_ASSERT(stack_.empty());
+            BOOST_ASSERT(closed_.size() == n_vertices);
+            BOOST_ASSERT(all_closed_rows_are_empty());
         }
 
 private:
@@ -142,12 +187,12 @@ private:
 
     //! Return whether a given vertex is blocked.
     bool is_blocked(Vertex v) const {
-        return get(blocked_, v) == blocked_true;
+        return get(blocked_, v) == blocked_true_color();
     }
 
     //! Block a given vertex.
     void block(Vertex v) {
-        put(blocked_, v, blocked_true);
+        put(blocked_, v, blocked_true_color());
     }
 
     //! Unblock a given vertex.
@@ -155,11 +200,11 @@ private:
         typedef typename ClosedMatrix::reference VertexList;
         typedef typename ClosedMatrix::value_type::iterator ClosedVertexIter;
 
-        put(blocked_, u, blocked_false);
+        put(blocked_, u, blocked_false_color());
         VertexList closed_to_u = closed_[index_of(u)];
 
         while (!closed_to_u.empty()) {
-            Vertex w = closed_to_u.back();
+            Vertex const w = closed_to_u.back();
             closed_to_u.pop_back();
             if (is_blocked(w))
                 unblock(w);
@@ -173,12 +218,18 @@ private:
         stack_.push_back(v);
         block(v);
 
+        // Cache some values that are used more than once in the function.
+        VertexIndex const index_of_start = index_of(start);
         AdjacentVertices const adj_vertices = GetAdjacentVertices()(v, graph_);
-        BOOST_FOREACH(Vertex w, adj_vertices) {
-            // Since we're only looking in the subgraph induced by `start` and
-            // the vertices with an index higher than `start`, we skip any
-            // vertex that does not satisfy that.
-            if (index_of(w) < index_of(start))
+        AdjacencyIterator const w_end = boost::end(adj_vertices);
+
+        for (AdjacencyIterator w_it = boost::begin(adj_vertices);
+                                                    w_it != w_end; ++w_it) {
+            Vertex const w = *w_it;
+            // Since we're only looking in the subgraph induced by `start`
+            // and the vertices with an index higher than `start`, we skip
+            // any vertex that does not satisfy that.
+            if (index_of(w) < index_of_start)
                 continue;
 
             // If the last vertex is equal to `start`, we have a circuit.
@@ -197,10 +248,12 @@ private:
         if (found_circuit)
             unblock(v);
         else
-            BOOST_FOREACH(Vertex w, adj_vertices) {
+            for (AdjacencyIterator w_it = boost::begin(adj_vertices);
+                                                    w_it != w_end; ++w_it) {
+                Vertex const w = *w_it;
                 // Like above, we skip vertices that are not in the subgraph
                 // we're considering.
-                if (index_of(w) < index_of(start))
+                if (index_of(w) < index_of_start)
                     continue;
 
                 // If `v` is not closed to `w`, we make it so.
@@ -222,10 +275,9 @@ private:
     Graph const& graph_;
     Visitor& visitor_;
     VertexIndexMap const& vim_;
-
+    Stack& stack_;
+    ClosedMatrix& closed_;
     BlockedMap blocked_;
-    ClosedMatrix closed_;
-    Stack stack_;
 };
 
 template <
@@ -234,19 +286,38 @@ template <
 >
 void call_hawick_circuits(Graph const& graph, Visitor /* by value */ visitor,
                           VertexIndexMap const& vertex_index_map) {
-
     typedef boost::graph_traits<Graph> Traits;
     typedef typename Traits::vertex_descriptor Vertex;
     typedef typename Traits::vertices_size_type VerticesSize;
+    typedef typename Traits::vertex_iterator VertexIterator;
+
+    typedef std::vector<Vertex> Stack;
+    typedef std::vector<std::vector<Vertex> > ClosedMatrix;
+
     typedef hawick_circuits_from<
-                Graph, Visitor, VertexIndexMap, GetAdjacentVertices
+                Graph, Visitor, VertexIndexMap, Stack, ClosedMatrix,
+                GetAdjacentVertices
             > SubAlgorithm;
 
     VerticesSize const n_vertices = num_vertices(graph);
-    BOOST_FOREACH(Vertex start, vertices(graph)) {
-        // We may NOT reuse sub_algo once it has been called.
-        SubAlgorithm sub_algo(graph, visitor, vertex_index_map, n_vertices);
-        sub_algo(boost::move(start));
+    Stack stack; stack.reserve(n_vertices);
+    ClosedMatrix closed(n_vertices);
+
+    VertexIterator start, last;
+    for (boost::tie(start, last) = vertices(graph); start != last; ++start) {
+        // Note1: The sub algorithm may NOT be reused once it has been called.
+
+        // Note2: We reuse the Stack and the ClosedMatrix (after clearing them)
+        // in each iteration to avoid redundant destruction and construction.
+        // It would be strictly equivalent to have these as member variables
+        // of the sub algorithm.
+        SubAlgorithm sub_algo(graph, visitor, vertex_index_map,
+                              stack, closed, n_vertices);
+        sub_algo(*start);
+        stack.clear();
+        typename ClosedMatrix::iterator row, last_row = closed.end();
+        for (row = closed.begin(); row != last_row; ++row)
+            row->clear();
     }
 }
 
